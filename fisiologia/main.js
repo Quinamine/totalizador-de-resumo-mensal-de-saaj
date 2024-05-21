9903b128f85e3d66e52500a6bfd23ee90b50c3d0
Adérito Félix Quinamine <dery.felix.df@gmail.com>
2024-05-20 21:54:43 -0300

    HTML: Actualizei o artigo 'Sobre' e 'Ajuda' (sobretudo Esvaziar ficha). CSS: Defini uma hierarquia de z-index para os elementos com position fixed. JS: Modifiquei o métodos do menu (Realçar totais, Esvaziar e Imprimir ficha), de backup da ficha na Local Storage e de totalização acrescentando variáveis e definindo nomes mais sugestivos.

--- fisiologia/main.js
+++ fisiologia/main.js
@@ -2,21 +2,21 @@
 
 const backup = {
     saveGridInputs() {
-        const gridInputs = document.querySelectorAll("[data-totalgeral]");
+        const inputsCelulares = document.querySelectorAll("[data-totalgeral]");
 
-        for (let i = 0; i < gridInputs.length; i++) {
+        for (let i = 0; i < inputsCelulares.length; i++) {
             
-            gridInputs[i].addEventListener("input", () => {
-                localStorage.setItem(`${keyPrefix}-input${i}`, gridInputs[i].value);
+            inputsCelulares[i].addEventListener("input", () => {
+                localStorage.setItem(`${keyPrefix}-input${i}`, inputsCelulares[i].value);
             });
-            gridInputs[i].value = localStorage.getItem(`${keyPrefix}-input${i}`);
+            inputsCelulares[i].value = localStorage.getItem(`${keyPrefix}-input${i}`);
         }
         
     },
     
     saveExtraInputs() {
-        const extraInputs = document.querySelectorAll(".input-nao-celular");
-        extraInputs.forEach( extraInput => {
+        const inputsNaoCelulares = document.querySelectorAll(".input-nao-celular");
+        inputsNaoCelulares.forEach( extraInput => {
             extraInput.addEventListener("input", () => localStorage.setItem(`${keyPrefix}-${extraInput.id}`, extraInput.value));
             extraInput.value = localStorage.getItem(`${keyPrefix}-${extraInput.id}`);
         });
@@ -25,23 +25,24 @@ const backup = {
 
 const totalizador = {
     filtrarEtotalizarCelulas(inputTarget) {
-        inputTarget.classList.add(`${inputTarget.dataset.totalparcial}`);
-        inputTarget.classList.add(`${inputTarget.dataset.totalgeral}`);
-    
-        // Total parcial
-        const totalParcial = document.querySelectorAll(`.${inputTarget.dataset.totalparcial}`);
-        const totalParcialOutput = document.querySelector(`.${inputTarget.dataset.totalparcialoutput}`);
-        totalParcialOutput.value = this.somar(totalParcial);
+         // Total parcial
+        let classNameDosOperandos = inputTarget.dataset.totalparcial;
+        inputTarget.classList.add(`${classNameDosOperandos}`);
+
+        let operandos = document.querySelectorAll(`.${classNameDosOperandos}`);
+        let celulaDeSaida = document.querySelector(`.${inputTarget.dataset.totalparcialoutput}`);
+        celulaDeSaida.value = this.somar(operandos);
     
         // Total Geral 
-        const totalGeral = document.querySelectorAll(`.${inputTarget.dataset.totalgeral}`);
-        const totalGeralOutput = document.querySelector(`.${inputTarget.dataset.totalgeraloutput}`);
-        totalGeralOutput.value = this.somar(totalGeral);
+        classNameDosOperandos = inputTarget.dataset.totalgeral;
+        inputTarget.classList.add(`${classNameDosOperandos}`);
+
+        operandos = document.querySelectorAll(`.${classNameDosOperandos}`);
+        celulaDeSaida = document.querySelector(`.${inputTarget.dataset.totalgeraloutput}`);
+        celulaDeSaida.value = this.somar(operandos);
 
         if(inputTarget.dataset.totalparcialnaoaplicaveloutput) {
-
             const totalParcialNPouput = document.querySelector(`.${inputTarget.dataset.totalparcialnaoaplicaveloutput}`); 
-
             totalParcialNPouput.value = 0;
         }
     },
@@ -57,10 +58,10 @@ const totalizador = {
 
 
 function escutarEventos() {
-    const gridInputs = document.querySelectorAll("[data-totalgeral]");
-    gridInputs.forEach( gi => {
-        gi.addEventListener("input", () => totalizador.filtrarEtotalizarCelulas(gi));
-        gi.value !== "" && totalizador.filtrarEtotalizarCelulas(gi);
+    const inputsCelulares = document.querySelectorAll("[data-totalgeral]");
+    inputsCelulares.forEach( inputCelular => {
+        inputCelular.addEventListener("input", () => totalizador.filtrarEtotalizarCelulas(inputCelular));
+        inputCelular.value !== "" && totalizador.filtrarEtotalizarCelulas(inputCelular);
     });
 }
 
--- fisiologia/multifuncoes.js
+++ fisiologia/multifuncoes.js
@@ -120,8 +120,8 @@ window.addEventListener("load", () => {
         alertarSobre(readonlyInputsMsg);
     }));
 
-    const gridInputs = document.querySelectorAll("[data-totalgeral]");
-    gridInputs.forEach (gi => gi.addEventListener("input", destacarCelulasSaturadas));
+    const inputsCelulares = document.querySelectorAll("[data-totalgeral]");
+    inputsCelulares.forEach (inputCelular => inputCelular.addEventListener("input", destacarCelulasSaturadas));
     destacarCelulasSaturadas();
 
     // Indicador nao aplicavel para APEs
